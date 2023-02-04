fc22dfeda593a8d86bb3d4cdeabf7fa0a27f2853
Rob Sokolowski <rpsoko@gmail.com>
2023-02-04 06:28:43 -0500

    Preparing for v2

---
+++ src/Evergreen/V2/Types.elm
@@ -0,0 +1,35 @@
+module Evergreen.V2.Types exposing (..)
+
+import Browser
+import Browser.Navigation
+import Url
+
+
+type alias FrontendModel =
+    { key : Browser.Navigation.Key
+    , message : String
+    , sourceText : String
+    }
+
+
+type alias BackendModel =
+    { message : String
+    }
+
+
+type FrontendMsg
+    = UrlClicked Browser.UrlRequest
+    | UrlChanged Url.Url
+    | NoOpFrontendMsg
+
+
+type ToBackend
+    = NoOpToBackend
+
+
+type BackendMsg
+    = NoOpBackendMsg
+
+
+type ToFrontend
+    = NoOpToFrontend
